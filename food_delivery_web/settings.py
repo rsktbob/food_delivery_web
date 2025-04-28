f90a0aa410d895a7e63a63660216472238ab7c14
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-04-29 02:36:15 +0800

    set up UI

--- food_delivery_web/settings.py
+++ food_delivery_web/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for food_delivery_web project.
 
@@ -122,7 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
